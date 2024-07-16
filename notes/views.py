eda4fb2a9fe7c4433c9af1709abe06cbf3321121
Yash Rajbhar <yashraj78289@gmail.com>
2024-07-16 18:50:36 +0530

    Changed function based views to more complex (list) class based views in notes app

--- notes/views.py
+++ notes/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from .models import Notes
 from django.http import Http404
+from django.views.generic import ListView, DetailView
 
-def list(request):
-        all_notes = Notes.objects.all()
-        return render(request, 'notes/notes_list.html',{'notes':all_notes})
+class NotesListView(ListView):
+       model = Notes
+       context_object_name = "notes"
+       template_name = "notes/notes_list.html"
 
-def detail(request, pk):
-        try:
-            note = Notes.objects.get(pk = pk)
-        except Notes.DoesNotExist:
-               raise Http404("The note doesn't exists")
-        return render(request, 'notes/notes_detail.html',{'note': note})
+
+
+# def detail(request, pk):
+#         try:
+#             note = Notes.objects.get(pk = pk)
+#         except Notes.DoesNotExist:
+#                raise Http404("The note doesn't exists")
+#         return render(request, 'notes/notes_detail.html',{'note': note})
+
+class NotesDetailView(DetailView):
+       model = Notes
+       context_object_name = "note"
